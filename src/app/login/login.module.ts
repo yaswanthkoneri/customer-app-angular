28956f3515313e0346c9196c5d77c71113d946b6
yaswanthkapollo <yaswanth.k@apollo247.org>
2021-06-02 21:23:05 +0530

    login page updated

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,13 +3,24 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login/login.component';
+import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+
 
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatIconModule,
   ]
 })
 export class LoginModule { }
