0b67eb9df810a137c4da1a75226e18e0f2295b36
yaswanthkapollo <yaswanth.k@apollo247.org>
2021-06-02 21:28:48 +0530

    feedback and product modules pushed

--- src/app/feedback/feedback.module.ts
+++ src/app/feedback/feedback.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FeedbackRoutingModule } from './feedback-routing.module';
+import { FeedbackComponent } from './feedback/feedback.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [FeedbackComponent],
   imports: [
     CommonModule,
     FeedbackRoutingModule
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
+import { ProductsComponent } from './products/products.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ProductsComponent],
   imports: [
     CommonModule,
     ProductsRoutingModule
