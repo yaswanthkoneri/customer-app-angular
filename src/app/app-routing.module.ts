3d228ba835e0e01c5b291531c0fe5400652bba42
yaswanthkapollo <yaswanth.k@apollo247.org>
2021-06-02 21:04:45 +0530

    login component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', loadChildren: ()=> import('./login/login.module').then((mod)=> mod.LoginModule)},
+  {path: 'list', loadChildren: ()=> import('./products/products.module').then((mod)=> mod.ProductsModule)},
+  {path: 'feedback', loadChildren: ()=> import('./feedback/feedback.module').then((mod)=> mod.FeedbackModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
