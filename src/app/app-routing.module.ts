db910cadf90c0542688ce29bbf21ce0eebf1e047
yaswanthkapollo <yaswanth.k@apollo247.org>
2021-06-02 21:31:49 +0530

    made login page as starting point of application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
+  { 
+    path: '', 
+    redirectTo: '/login', 
+    pathMatch: 'full' 
+  },
   {path: 'login', loadChildren: ()=> import('./login/login.module').then((mod)=> mod.LoginModule)},
   {path: 'list', loadChildren: ()=> import('./products/products.module').then((mod)=> mod.ProductsModule)},
   {path: 'feedback', loadChildren: ()=> import('./feedback/feedback.module').then((mod)=> mod.FeedbackModule)}
